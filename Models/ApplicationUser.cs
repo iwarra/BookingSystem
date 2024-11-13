b6618dae807f2907a82b302334a5278334086f0d
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-11-13 13:40:21 +0100

    Added classes and navigation properties

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BookingSystem.Models
+{
+    public class ApplicationUser : IdentityUser 
+    {
+        public ICollection<ApplicationUserGymClass> AttendingUsers { get; set; }
+    }
+}
---
+++ Models/ApplicationUserGymClass.cs
@@ -0,0 +1,6 @@
+﻿namespace BookingSystem.Models
+{
+    public class ApplicationUserGymClass
+    {
+    }
+}

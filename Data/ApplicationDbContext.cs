38e6302e48c069221df6bf85e6b019d4de83eff2
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-11-13 13:30:48 +0100

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BookingSystem.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingSystem.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GymClass> GymClasses { get; set; } = default!;
+    }
+}

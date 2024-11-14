64620cb9fffbebd025251916e033f8f89e001581
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-11-14 16:06:05 +0100

    View model for details

---
+++ Models/ViewModels/DetailsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookingSystem.Models.ViewModels
+{
+    public class DetailsViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [Display(Name = "Starts at")]
+        public DateTime StartTime { get; set; }
+        public TimeSpan Duration { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Description { get; set; } = string.Empty;
+
+        public List<string> AttendingMemberEmails { get; set; } = new List<string>();
+
+    }
+}
